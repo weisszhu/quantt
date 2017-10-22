0a27966d221db11d6c1243fbad05b7279eb206e2
maxpmx <32996899+maxpmx@users.noreply.github.com>
2017-10-22 21:49:09 +0800

    data

---
+++ data.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+from WindPy import *
+import numpy as np
+import pandas as pd
+import datetime,time
+reload(sys)
+sys.setdefaulttencoding('utf-8')
+
+
+def get_one_stock_data(stock,start_day,end_day):
+    w.start()
+    data = w.wsd()
+    one_data = pd.DataFrame()
+    one_data['name'] = stock
+    one_data['date'] = data.Data[0]
+    one_data['open'] = data.Data[1]
+    return one_data
+
+
+def begin_store_day_data(stocklist,start_day,end_day):
+    whole_data = pd.DataFrame()
+    for stock in stocklist:
+        temp = get_one_stock_data(stock,start_day,end_day)
+        #合并temp和whole_data-
+
+    return whole_data
+
+if __name__ == '__main__':
+    stocklist = ['asdfadsf']
+    start_day = '2011-01-01 00:00:00'
+    end_day = ''
+    print begin_store_day_data(stocklist,start_day,end_day)
